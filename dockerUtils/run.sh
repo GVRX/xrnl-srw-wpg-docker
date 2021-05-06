e057e0907ba71c61a60291e151ee736c56cd7bf8
GVRX <g.vanriessen@latrobe.edu.au>
2021-05-06 21:45:50 +1000

    change run script to accept command line arguments

--- dockerUtils/run.sh
+++ dockerUtils/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+
+# usage:  sh run.sh <name-to-give-container> <name-of-image> <command to run (optional>
+
 docker run -t -d --name $1 \
        --mount src="$(pwd)/xl",target=/opt/xl,type=bind \
        --mount src="$(pwd)/data",target=/data,type=bind \
        --mount src="$(pwd)/xrnl",target=/opt/xrnl,type=bind \
-       $2 
+       $2 \
+       $3 
 
